54f0910177ca3c3dce0958c5a9180f86a95f233b
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-08 16:28:55 +0530

    handled the events

--- lib/src/features/weather/presentation/bloc/weather_bloc.dart
+++ lib/src/features/weather/presentation/bloc/weather_bloc.dart
@@ -1,14 +1,26 @@
 
   import 'package:flutter_bloc/flutter_bloc.dart';
   import 'package:meta/meta.dart';
+import 'package:weather_app_bloc/src/features/weather/data/models/models.dart';
+import 'package:weather_app_bloc/src/features/weather/domain/repositories/repositories.dart';
   
   part 'weather_event.dart';
   part 'weather_state.dart';
   
+  // Repo & this Bloc would connect and handle everything
   class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-    WeatherBloc() : super(WeatherInitial()) {
-      on<WeatherEvent>((event, emit) {
-        // TODO: implement event handler
-      });
+    final WeatherRepository weatherRepository;
+    WeatherBloc(this.weatherRepository) : super(WeatherInitial()) {
+      on<WeatherFetched>((_getCurrentWeather));
+    }
+    void _getCurrentWeather(WeatherFetched event, Emitter<WeatherState> emit,) async {
+      emit (WeatherLoading());
+      try {
+        final weather = await weatherRepository.getCurrentWeather();
+        emit(WeatherSuccess(weatherModel: weather));
+      }
+      catch (e) {
+        emit(WeatherFailure(e.toString()));
+      }
     }
   }
