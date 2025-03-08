f2896d7180c6f80831edcf5e5369063fd5dd190a
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-08 16:28:09 +0530

    fixes

--- lib/src/features/weather/domain/repositories/repositories.dart
+++ lib/src/features/weather/domain/repositories/repositories.dart
@@ -1,5 +1,29 @@
- 
-    abstract class WeatherRepository {
-        // Future<User> getUser(String userId);
+import 'dart:convert';
+
+import 'package:weather_app_bloc/src/features/weather/data/implements/weather_data_provider.dart';
+import 'package:weather_app_bloc/src/features/weather/data/models/models.dart';
+
+class WeatherRepository {
+  final WeatherDataProvider weatherDataProvider;
+
+  WeatherRepository(
+    this.weatherDataProvider
+    );
+
+  Future<WeatherModel> getCurrentWeather() async {
+    try {
+      String cityName = 'London';
+      final weatherData = await weatherDataProvider.getCurrentWeather(cityName);
+
+      final data = jsonDecode(weatherData);
+
+      if (data['cod'] != '200') {
+        throw 'An unexpected error occurred';
       }
-    
+
+      return WeatherModel.fromMap(data);
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+}
