f1088844b4133e35fb881cdd4dd209d633c929b6
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-08 16:28:25 +0530

    defined the states

--- lib/src/features/weather/presentation/bloc/weather_state.dart
+++ lib/src/features/weather/presentation/bloc/weather_state.dart
@@ -5,4 +5,16 @@
   abstract class WeatherState {}
   
   class WeatherInitial extends WeatherState {}
+
+  final class WeatherSuccess extends WeatherState{
+    final WeatherModel weatherModel;
+    WeatherSuccess({required this.weatherModel});
+  }
+
+  final class WeatherFailure extends WeatherState{
+    final String error;
+    WeatherFailure(this.error);
+  }
+
+  final class WeatherLoading extends WeatherState{}
   
