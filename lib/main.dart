2633ef85e09d653c8c92b1b9eaf3d1067d7f4be7
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-08 16:46:45 +0530

    bloc provider used

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app_bloc/src/features/weather/data/implements/weather_data_provider.dart';
+import 'package:weather_app_bloc/src/features/weather/domain/repositories/repositories.dart';
+import 'package:weather_app_bloc/src/features/weather/presentation/bloc/weather_bloc.dart';
 import 'package:weather_app_bloc/src/features/weather/presentation/pages/weather_screen.dart';
 
 void main() {
@@ -8,12 +12,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+//Here Repository would be passed and could be accessed by BLoC
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(
+    return RepositoryProvider(
+      create: (context) => WeatherRepository(WeatherDataProvider()),
+      child: BlocProvider(
+        create: (context) => WeatherBloc(
+          context.read<WeatherRepository>()
+        ),
+        child: MaterialApp(
+          theme: ThemeData.dark(
+          ),
+          home: const WeatherScreen()
+        ),
       ),
-      home: const WeatherScreen()
     );
   }
 }
--- lib/src/features/weather/data/implements/implements.dart
+++ lib/src/features/weather/data/implements/implements.dart
@@ -1,11 +1,25 @@
 
-    import '../sources/sources.dart';
+    import 'package:weather_app_bloc/src/features/weather/data/implements/weather_data_provider.dart';
+
+import 'package:weather_app_bloc/src/features/weather/data/models/models.dart';
+
+import '../sources/sources.dart';
     import '../../domain/repositories/repositories.dart';
     
     class WeatherRepositoryImp implements WeatherRepository{
 
         final WeatherRemoteDataSource remoteDataSource;
         WeatherRepositoryImp({required this.remoteDataSource});
+
+  @override
+  Future<WeatherModel> getCurrentWeather() {
+    // TODO: implement getCurrentWeather
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement weatherDataProvider
+  WeatherDataProvider get weatherDataProvider => throw UnimplementedError();
       
         // ... example ...
         //
